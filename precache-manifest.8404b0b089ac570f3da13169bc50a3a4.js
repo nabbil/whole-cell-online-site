2003954c8e2cee1af751ae35f3c3bdd546f421f0
ml-aakash-dev <ml.aakash.dev@gmail.com>
2020-05-05 04:30:43 +0500

    Updates

---
+++ precache-manifest.8404b0b089ac570f3da13169bc50a3a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1242ea8260eb2527d2500e8db775df7f",
+    "url": "/whole-cell-online-site/index.html"
+  },
+  {
+    "revision": "e71ad5f3e48c1075b969",
+    "url": "/whole-cell-online-site/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0060774358b404f920e3",
+    "url": "/whole-cell-online-site/static/js/2.90931efe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whole-cell-online-site/static/js/2.90931efe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71ad5f3e48c1075b969",
+    "url": "/whole-cell-online-site/static/js/main.6fff4025.chunk.js"
+  },
+  {
+    "revision": "6f3218be17cea54cb5ad",
+    "url": "/whole-cell-online-site/static/js/runtime-main.e0f45547.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/whole-cell-online-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
